ff263a84180ddd5a1d3d51970b5e94eea880d9ab
abhisheksaha1995 <abhisheksaha467@gmail.com>
2025-11-22 21:14:32 +0530

    Addnewcat

--- src/test/java/objects/AddNewCategory.java
+++ src/test/java/objects/AddNewCategory.java
@@ -3,13 +3,23 @@ package objects;
 import base.Main;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class AddNewCategory extends Main {
-    WebDriver driver;
+   
 
-    public AddNewCategory(WebDriver driver){
-        this.driver = driver;
+     
+    public static WebElement prod_cat(WebDriver driver) {
+        return driver.findElement(By.xpath("//span[@class='menu-title' and contains(text(), 'Products')]"));
+        
+        	
     }
-
-    By prod_cat = By.xpath("//span[@class='menu-title' and contains(text(), 'Products')]");
+    public static WebElement click_cat(WebDriver driver) {
+    	return driver.findElement(By.xpath("//*[@id=\"Products\"]/ul/li[1]/a"));
+    	
+    }
+    
+    
+    
+    
 }
